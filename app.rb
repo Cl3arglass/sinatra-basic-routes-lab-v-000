337b98b11a82814ea9118df5b0333ae56ce674ea
Holden Altaffer <haltaffer@gmail.com>
2018-06-13 07:45:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,4 +4,13 @@ class App < Sinatra::Base
   get '/name' do
   "My name is Piton"
   end
+
+  get '/hometown' do
+  "My hometown is Paris"
+  end
+
+  get '/favorite-song' do
+  "My favorite song is Tiny Dancer"
+  end
+
 end
